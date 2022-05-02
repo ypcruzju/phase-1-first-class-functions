8cf327b205a1e7d91ce0aa8dd4817bcae56776c0
yaxpaccruzjuarez <ypjuarez11@gmail.com>
2022-05-02 13:59:19 -0400

    commit tree 2a80118a0097ef560f8029ba9c7240214d6977b5

---
+++ index.js
@@ -0,0 +1,20 @@
+function ri(){
+    return ` By FENTY`
+
+
+}
+
+function receivesAFunction(callback){
+   callback + ri()
+}
+
+receivesAFunction(`Mimosa Sunrise`)
+
+
+function returnsANamedFunction(){
+    return receivesAFunction 
+}
+
+returnsAnAnonymousFunction = dualKit => ri
+
+
